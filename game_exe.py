c6175e9288d7b2763120705370abfb36d9ba12cb
Carlo <cvcarnap@gmail.com>
2024-01-17 22:06:27 +0100

    Added leaderboard.

--- game_exe.py
+++ game_exe.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from questions import *
 
 
@@ -17,11 +18,28 @@ def get_answers(answer_tuple):
 
 # enter name and score to the leaderboard
 def add_leaderboard_entry(player_name, score):
+    with open("leaderboard.txt", "a") as leaderboard:
+        leaderboard.write(f"{score[0]},{player_name} ({score[1]})\n")
     return
 
 
 # prints out leaderboard
-def get_leaderboard():
+def print_leaderboard():
+    leaderboard = np.loadtxt('./leaderboard.txt', delimiter = ',', dtype = str)
+    dick_of_leaders = {}
+    # add players to the dictionary of leaders
+    for entry in leaderboard:
+        key_number = int(entry[0])
+        if key_number in dick_of_leaders.keys():                            # append player to his score entry
+            dick_of_leaders[key_number] = dick_of_leaders[key_number] + ', ' +  entry[1]
+        else:                                                               # add a players score entry
+            dick_of_leaders[key_number] = entry[1]
+    # handle and print out the dictionary
+    dick_of_leaders_reverse_sorted = dict(reversed(sorted(dick_of_leaders.items())))
+    print("Here are the current leaders (number of correct answers, plus names with amount of available questions in brackets):")
+    for key, value in dick_of_leaders_reverse_sorted.items():
+        print(f"   {key}\t{value}")
+    print()
     return
 
 
@@ -37,7 +55,7 @@ n_questions = len(questions_dick)                                           # nu
 # introduction to the program
 print("Please enter your name: ")
 player_name = input()
-print(f"\nWelcome to Kern- / Teilchenphysik, {player_name}, I want to play a game with you. We have a total of {n_questions} questions to answer for you!\n\n")
+print(f"\n +++ Welcome to Kern- / Teilchenphysik, {player_name}, I want to play a game with you. We have a total of {n_questions} questions to answer for you! +++ \n\n")
 
 # iterating through all question until an error is made
 i_questions = 0
@@ -47,7 +65,6 @@ while (i_questions < n_questions):
     print(f"Question ({i_questions+1}/{n_questions}): {questions_list_entry[0]}")
     correct_answer_letter = get_answers(questions_list_entry[1])
     print("\nPlease enter your answer ('a', 'b', 'c', 'd') or quit ('q'):")
-
     # enter answer
     command = input()
     if command == '-q' or command == 'quit' or command == 'q':
@@ -56,8 +73,14 @@ while (i_questions < n_questions):
     elif command == correct_answer_letter:
         print(f"\n +++ CORRECT! You are at {i_questions+1} out of {n_questions}. +++ \n")
     else:
-        print(f"\n +++ FALSE! You are a loser, you scored {i_questions} out of {n_questions}. +++ \n")
+        print(f"\n +++ FALSE! You are a loser, {correct_answer_letter.upper()} was correct. You scored {i_questions} out of {n_questions}. +++ \n")
         break
-
     # initiate next iteration step
     i_questions += 1
+
+
+# +++ program end +++
+
+# add name to leaderboard and print it out
+add_leaderboard_entry(player_name, (i_questions, n_questions))
+print_leaderboard()
